59f5d6b203548e9cffbe1e18b078ab79b0f096fa
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-03-21 20:43:31 +0300

    uygulama sonu

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Ilham Ver'),
     );
   }
 }
@@ -30,17 +29,63 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-
+    var ekranBilgisi = MediaQuery.of(context);
+    final double ekranYuksekligi = ekranBilgisi.size.height;
+    final double ekranGenisligi = ekranBilgisi.size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Column(
         children: [
-
+          Padding(
+            padding: EdgeInsets.only(top: ekranYuksekligi/100,bottom: ekranYuksekligi/100),
+            child: SizedBox(
+              width: ekranGenisligi/4,
+              child: Image.asset("images/stevejobs@2x.png")
+              ),
+          ),
+          Text(
+            "Steve Jobs",
+            style: TextStyle(
+              color: Colors.redAccent,
+              fontWeight: FontWeight.bold,
+              fontSize: ekranGenisligi/25,
+            ),
+            ),
+            const Spacer(),
+          Padding(
+            padding: EdgeInsets.only(left: ekranGenisligi/100,right: ekranGenisligi/100),
+            child: Text(
+              "Dunyayi degistirecek insanlar, onu degistirebileceklerini dusunecek kadar cilgin olanlardir.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+              color: Colors.black45,
+              fontSize: ekranGenisligi/30,
+              fontWeight: FontWeight.w500,
+              ),
+                ),
+          ),
+          const Spacer(),
+          Padding(
+            padding:EdgeInsets.only(bottom: ekranYuksekligi/100),
+            child: SizedBox(
+              width: ekranGenisligi/2,
+              height: ekranYuksekligi/15,
+              child: ElevatedButton(
+                child: Text("ILHAM VER", style: TextStyle(color: Colors.white,fontSize: ekranGenisligi/25),),
+                onPressed: () {
+                    print("ilham Verildi");
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green,
+                    
+                  ),
+                  ),
+            ),
+          )
         ],
       ),
     );
